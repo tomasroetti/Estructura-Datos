65e03fb3820d7c13504ffda7d1ec7412d7778c2d
tomasroetti <roettitomas@gmail.com>
2022-05-02 19:56:45 -0300

    Condiciones

--- src/EstructuraDeDatos.java
+++ src/EstructuraDeDatos.java
@@ -3,13 +3,25 @@ import java.util.Scanner;
 public class EstructuraDeDatos {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Cuál es tu nombre ?");
-        String nombre = teclado.nextLine();
-        System.out.println("Cual es tu edad ?");
-        int edad = teclado.nextInt();
-        System.out.println("Cual es tu número de telefono ?");
-        int telefono = teclado.nextInt();
-        System.out.printf("Tu nombre es %s tenes %d años y tu celular es %d", nombre, edad, telefono);
+        System.out.println("Calculadora de propinas");
+        System.out.println("Ingrese la cantidad a pagar");
+        double cantpropinas = teclado.nextDouble();
+        System.out.println();
+        System.out.println("Ingrese el % de propina");
+        int porcentaje = teclado.nextInt();
+        double total = 0.0D;
+        if (porcentaje < 15) {
+            System.out.println("El servicio no fue bueno ");
+            total = cantpropinas / (double)porcentaje * 100.0D;
+            System.out.printf("El total de propina es de %.1f ", total);
+        } else if (15 <= porcentaje && porcentaje < 30) {
+            total = cantpropinas / (double)porcentaje * 100.0D;
+            System.out.printf("El total de propina es de %.1f ", total);
+        } else {
+            System.out.println("El servicio fue excelente");
+            total = cantpropinas / (double)porcentaje * 100.0D;
+            System.out.printf("El total de propina es de %.1f ", total);
+        }
 
     }
 
