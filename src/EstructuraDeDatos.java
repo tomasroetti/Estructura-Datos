4db6b00162cbf5bbf5d22d92b4aba9a89740269f
tomasroetti <roettitomas@gmail.com>
2022-05-02 20:02:29 -0300

    CuartaActividad

--- src/EstructuraDeDatos.java
+++ src/EstructuraDeDatos.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class EstructuraDeDatos {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("¿Cúal es tu nombre?");
-        String nombre = teclado.nextLine();
-        System.out.println("Tu nombre es " + nombre);
-        System.out.println("Cuál es tu edad??");
-        int edad = teclado.nextInt();
-        System.out.println("Tu edad es " + edad + " años");
+        System.out.println("Cuántos número queres ingresar?");
+        int numero = teclado.nextInt();
+        System.out.printf("Ingrese %d datos\n", numero);
+
+        for(int contador = 1; contador <= numero; ++contador) {
+            System.out.printf("Dato  %d: ", contador);
+            int entrada = teclado.nextInt();
+            int resultado = entrada * 2;
+            System.out.printf("El dato procesado vale %d\n", resultado);
+        }
+
     }
 
 
