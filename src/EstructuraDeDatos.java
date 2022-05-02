8a4532cd44f17040c7e3d1af1b67c29a6bb24945
tomasroetti <roettitomas@gmail.com>
2022-05-02 20:01:50 -0300

    Scanner

--- src/EstructuraDeDatos.java
+++ src/EstructuraDeDatos.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 public class EstructuraDeDatos {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("¿Cúal es tu nombre?");
+        String nombre = teclado.nextLine();
+        System.out.println("Tu nombre es " + nombre);
+        System.out.println("Cuál es tu edad??");
+        int edad = teclado.nextInt();
+        System.out.println("Tu edad es " + edad + " años");
     }
 
 
